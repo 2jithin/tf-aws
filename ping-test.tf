3cb5a537eb800b5e84accbc83fe806fb6004f437
Jithinc1993 <jithincotp@gmail.com>
2023-05-16 14:37:25 +0530

    fixed spell error

--- ping-test.tf
+++ ping-test.tf
@@ -3,5 +3,5 @@ module "ping_test" {
   source                = "./ping-test"
   instance_ids          = aws_instance.ec2-instance.*.public_ip
   path_to_private_key   = var.path_to_private_key
-  private_id            = aws_instance.ec2-instance.*.private_ip
+  private_ip           = aws_instance.ec2-instance.*.private_ip
 }
