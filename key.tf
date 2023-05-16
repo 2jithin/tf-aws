fe528d081f45814a7a1fa84b55fa89da5c5184f0
Jithinc1993 <jithincotp@gmail.com>
2023-05-16 15:36:42 +0530

    reverted

--- key.tf
+++ key.tf
@@ -3,7 +3,6 @@ resource "aws_key_pair" "mykeypair" {
   public_key = file(var.path_to_public_key)
   lifecycle {
     ignore_changes = [ key_name , public_key ]
-    delete_before_replace = true
   }
 }
 
