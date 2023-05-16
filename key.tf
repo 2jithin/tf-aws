13f28766d16dd05e306475eb105429813d52ac76
Jithinc1993 <jithincotp@gmail.com>
2023-05-16 15:34:48 +0530

    modified

--- key.tf
+++ key.tf
@@ -1,6 +1,10 @@
 resource "aws_key_pair" "mykeypair" {
   key_name   = "mykeypair"
   public_key = file(var.path_to_public_key)
+  lifecycle {
+    ignore_changes = [ key_name , public_key ]
+    #delete_before_replace = true
+  }
 }
 
 output "keypair" {
