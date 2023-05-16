fb87ea44bd1fe597c5889bb5c7c8bcf9021a6330
Jithinc1993 <jithincotp@gmail.com>
2023-05-16 16:03:31 +0530

    updated

--- locals.tf
+++ locals.tf
@@ -2,5 +2,7 @@
 locals {
   unique_stage        = lookup(var.stage_mapping, lower(var.stage)).unique_stage
   environment         = var.stage
+  timestamp           = timestamp()
   timestamp_sanitized = replace("${local.timestamp}", "/[-| |T|Z|:]/", "")
+
 }
